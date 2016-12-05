7a43bd957cf99a80ddbb66f07a397546d2466816
Steven Childrey <steven.childrey36@yahoo.com>
2016-12-05 15:32:26 -0500

    Remove/Add brandItem on scroll

--- protfolioJS.js
+++ protfolioJS.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
   
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      var img = document.getElementById('headerIMG')
-      var height = img.clientHeight;
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > height) {
-      $('nav').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < height + 1) {
-      $('nav').removeClass('navbar-fixed');
-    }
-  });
+    $(window).scroll(function () {
+            //if you hard code, then use console
+            //.log to determine when you want the 
+            //nav bar to stick.  
+            var img = document.getElementById('headerIMG')
+            var height = img.clientHeight;
+            console.log($(window).scrollTop())
+        if ($(window).scrollTop() > height) {
+            $('nav').addClass('navbar-fixed');
+            $('#brandItem').removeClass('hide');
+        }
+        if ($(window).scrollTop() < height + 1) {
+            $('nav').removeClass('navbar-fixed');
+            $('#brandItem').addClass('hide');
+        }
+    });
 });
